df2a33f9298a93110523c40c4d404278d322e060
Mac <mac@mb-new2019.home>
2020-06-15 15:31:36 +0300

    frontend clean skeleton
    Signed-off-by: Mac <mac@mb-new2019.home>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
-import HomePageView from 'containers/HomePageView';
 import 'resources/scss/style.scss';
 
-export const App = withRouter(() => {
-    return (
-        <Switch>
-            <Route path="/" exact={true} component={HomePageView} />
-        </Switch>
-    );
-});
+export const App = () => {
+    return <div>test</div>;
+};
